9daae15beacb5d787123e64d7ebbbf3fa5ae47d6
russ-b1 <80778094+russ-b1@users.noreply.github.com>
2021-04-07 21:14:42 -0400

    Create garageexample.java

---
+++ garageexample.java
@@ -0,0 +1,16 @@
+/* 
+Garage problem in class
+2-23-17 */
+
+public class GarageExample {// opens class
+   public static main(String[] args) {//opens main
+   
+   double ticket;
+   
+   ticket = ticketValue( 
+   
+   
+   
+   } //closes main
+   
+   } //closes class
