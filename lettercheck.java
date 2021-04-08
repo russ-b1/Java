6608058c2933599a11b53c0b032b05bc0d8b77d3
russ-b1 <80778094+russ-b1@users.noreply.github.com>
2021-04-07 21:12:01 -0400

    Create lettercheck.java

---
+++ lettercheck.java
@@ -0,0 +1,24 @@
+/* ITM 251
+4/20/17
+Final Project
+
+*/
+
+import java.util.Scanner;
+
+public class LetterCheck {//opens LetterCheck
+
+public static final int guessAttempts = 6;
+public string letterGuess;
+public string letterWrong;
+public string letterCorrect;
+
+
+public static void  {//opens method
+
+
+
+
+}//closes method 
+
+} //closes LetterCheck
